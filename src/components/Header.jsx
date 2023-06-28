f4bce17910d0b654bcae2d05686f6f1861264f44
jacekzielinski87 <106507367+jacekzielinski87@users.noreply.github.com>
2023-06-28 14:58:02 +0200

    Add some properties

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
     <header className='header'>
       <nav className='navigation'>
         <ul className='nav_item'>
-          <li className='links'><Link className='link' to="/">O mnie</Link></li>
-          <li className='links'><Link className='link' to="/Kategorie">Kategorie</Link></li>
-          <li className='links'><Link className='link' to="/Wywiady">Wywiady</Link></li>
-          <li className='links'><Link className='link' to="/Kontakt">Kontakt</Link></li>
+          <li className='link'><Link className='links' to="/">O mnie</Link></li>
+          <li className='link'><Link className='links' to="/Kategorie">Kategorie</Link></li>
+          <li className='link'><Link className='links' to="/Wywiady">Wywiady</Link></li>
+          <li className='link'><Link className='links' to="/Kontakt">Kontakt</Link></li>
         </ul>
       </nav>
     </header>
